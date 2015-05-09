4342d464ec082efa28e02da7875ae5095050e46b
Even <evva-gangsta@hotmail.com>
2015-05-09 21:09:21 +0200

    Update AnsattProfil.java

--- Forsikring/AnsattProfil.java
+++ Forsikring/AnsattProfil.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -7,33 +8,34 @@ public class AnsattProfil extends JFrame
     private Register register;
     private Ansatt ansatt;
     private MainListener lytter;
-    //private WinListener wLytter;
     private BorderLayout layout;
-    //private PanelStack panel;
     private Container c;
+    private Font font, font2;
     private JButton rediger, lagre;
-    private JLabel bilde, navnLabel, fnavn, enavn, persnr, pnrLabel, tlf, tlfLabel, avd, avdLabel, ansid, idLabel;
-    private String navn1, navn2, tlfnr, avdeling;
-    private JTextField fonavn, etnavn, telf, avde;
-    private JPanel editPanel, savePanel, knappePanel, bildePanel, profilen, info, navnPanel, tlfPanel, avdPanel;
+    private JLabel navn, bilde, fnLab, navnLabel, idLabel, nrLabel,
+    		tlfLabel, avdLabel, persnr, ansid, tlf, avd;
+    private String aid, penr, navn1, navn2, tlfnr, avdeling;
+    private JTextField fnavn, enavn, telf, avde;
+    private JPanel editPanel, savePanel, knappePanel, bildePanel, endaPanel, profilen, info, navnPanel, tlfPanel, avdPanel;
 
     public AnsattProfil(Ansatt anna)
     {
         super("Ansatt");
         ansatt = anna;
         lytter = new MainListener();
-        Font font = new Font("SansSerif", Font.BOLD, 15);
+        font = new Font("SansSerif", Font.BOLD, 20);
+        font2 = new Font("SansSerif", Font.BOLD, 15);
 
         editPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         rediger = new JButton("Rediger");
-        rediger.setFont(font);
+        rediger.setFont(font2);
         rediger.addActionListener(lytter);
         rediger.setEnabled(true);
         editPanel.add(rediger);
 
         savePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         lagre = new JButton("Lagre");
-        lagre.setFont(font);
+        lagre.setFont(font2);
         lagre.addActionListener(lytter);
         lagre.setEnabled(false);
         savePanel.add(lagre);
@@ -41,6 +43,7 @@ public class AnsattProfil extends JFrame
         knappePanel = new JPanel( new BorderLayout());
         knappePanel.add(editPanel, BorderLayout.LINE_START);
         knappePanel.add(savePanel, BorderLayout.LINE_END);
+        knappePanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
         c = getContentPane();
         layout = new BorderLayout();
@@ -48,75 +51,95 @@ public class AnsattProfil extends JFrame
         c.setBackground(Color.white);
 
 		//bilde = new JPanel(new ImageIcon( getClass().getResource("bilder/" + dyrenavn[dyrevelger.getSelectedIndex()] + ".gif")));
-        bilde = new JLabel(new ImageIcon( getClass().getResource("bilder/kong.jpg")));
-		bildePanel = new JPanel(new BorderLayout(20,20));
-		bildePanel.add(bilde);
-		bildePanel.setPreferredSize(new Dimension(230,150));
-		bildePanel.setMaximumSize(new Dimension(230,150));
+        bilde = new JLabel(new ImageIcon( getClass().getResource("bilder/bilde.png")));
+		bildePanel = new JPanel(new BorderLayout());
+		bildePanel.add(bilde, BorderLayout.PAGE_START);
+		bildePanel.setPreferredSize(new Dimension(130,130));
+		bildePanel.setMaximumSize(new Dimension(130,130));
 
-        ansid = new JLabel(ansatt.getAnsattNr());
-        ansid.setFont(font);
+		endaPanel = new JPanel(new BorderLayout());
+		endaPanel.add(bildePanel, BorderLayout.PAGE_START);
+
+
+		aid = ansatt.getAnsattNr();
+        penr = ansatt.getPersonNr();
         navn1 = ansatt.getFornavn();
         navn2 = ansatt.getEtternavn();
-        fnavn = new JLabel(navn1);
-        fnavn.setFont(font);
-        enavn = new JLabel(navn2);
-		enavn.setFont(font);
-		persnr = new JLabel(ansatt.getPersonNr());
-		persnr.setFont(font);
 		tlfnr = ansatt.getTelefonNr();
+		avdeling = ansatt.getAvdeling();
+
+		idLabel = new JLabel("Ansattnr");
+		idLabel.setFont(font2);
+		ansid = new JLabel(aid);
+		ansid.setFont(font);
+		nrLabel = new JLabel("Personnr");
+		nrLabel.setFont(font2);
+		persnr = new JLabel(penr);
+		persnr.setFont(font);
+		navnLabel = new JLabel("Navn");
+		navnLabel.setFont(font2);
+		navn = new JLabel(navn1 + " " + navn2);
+		navn.setFont(font);
+		tlfLabel = new JLabel("Telefon");
+		tlfLabel.setFont(font2);
 		tlf = new JLabel(tlfnr);
 		tlf.setFont(font);
-		avdeling = ansatt.getAvdeling();
+		avdLabel = new JLabel("Avdeling");
+		avdLabel.setFont(font2);
 		avd = new JLabel(avdeling);
 		avd.setFont(font);
 
-		fonavn = new JTextField(navn1);
-		fonavn.setFont(font);
-		fonavn.setVisible(false);
-		etnavn = new JTextField(navn2);
-		etnavn.setFont(font);
-		etnavn.setVisible(false);
+		fnavn = new JTextField(navn1);
+		fnavn.setFont(font);
+		enavn = new JTextField(navn2);
+		enavn.setFont(font);
 		telf = new JTextField(tlfnr);
 		telf.setFont(font);
-		telf.setVisible(false);
 		avde = new JTextField(avdeling);
 		avde.setFont(font);
-		avde.setVisible(false);
 
 		navnPanel = new JPanel(new GridLayout(2,2));
 		navnPanel.add(fnavn);
 		navnPanel.add(enavn);
-		navnPanel.add(fonavn);
-		navnPanel.add(etnavn);
-
+		/*
 		tlfPanel = new JPanel(new GridLayout(1,1));
 		tlfPanel.add(tlf);
-		tlfPanel.add(telf);
 
 		avdPanel = new JPanel(new GridLayout(1,1));
-		avdPanel.add(avd);
-		avdPanel.add(avde);
+		avdPanel.add(avd);*/
 
-        info = new JPanel(new GridLayout(5,1));
+        info = new JPanel(new GridLayout(11,1));
+        info.add(idLabel);
 		info.add(ansid);
+		info.add(nrLabel);
 		info.add(persnr);
-		info.add(navnPanel);
-		info.add(tlfPanel);
-		info.add(avdPanel);
+		info.add(navnLabel);
+		info.add(navn);
+		info.add(tlfLabel);
+		info.add(tlf);
+		info.add(avdLabel);
+		info.add(avd);
+		info.setBorder(new EmptyBorder(new Insets(5, 20, 20, 5)));
+		info.setBackground(Color.white);
 
-        profilen = new JPanel(new FlowLayout());
+
+        profilen = new JPanel(new BorderLayout(10,10));
+        profilen.setPreferredSize(new Dimension(200, 500));
+        profilen.setMaximumSize(new Dimension(200, 500));
+        //profilen.add(bildePanel, BorderLayout.WEST);
+        //profilen.add(endaPanel, BorderLayout.WEST);
         profilen.add(bildePanel, BorderLayout.PAGE_START);
         profilen.add(info, BorderLayout.CENTER);
-        profilen.setBackground(Color.white);
+        //profilen.setBackground(Color.white);
 
-        //c.add(mittenPos, BorderLayout.CENTER);
+        //c.add(profilen, BorderLayout.CENTER);
+        JPanel flyt = new JPanel( new FlowLayout(FlowLayout.CENTER));
+        flyt.add(profilen);
+        setLayout(new BorderLayout());
         c.add(knappePanel, BorderLayout.PAGE_START);
-        c.add(profilen, BorderLayout.LINE_START);
-        //c.add(bildePanel, BorderLayout.LINE_START);
-        //c.add(info, BorderLayout.CENTER);
+        //c.add(new JScrollPane(flyt), BorderLayout.CENTER);
+		c.add(flyt, BorderLayout.CENTER);
 
-        //addWindowListener(wLytter);
         setDimensjon();
         setLocationRelativeTo(null);
         setVisible(true);
@@ -124,45 +147,71 @@ public class AnsattProfil extends JFrame
     }
     private void setDimensjon()
     {
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        /*Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenSize = toolkit.getScreenSize();
         int høyde = screenSize.height;
         int bredde = screenSize.width;
         double hReduction = 0.40, vReduction = 0.30;
-        setSize((int)(bredde*vReduction), (int)(høyde*hReduction) );
+        setSize((int)(bredde*vReduction), (int)(høyde*hReduction) );*/
+        int høyde = 400, bredde = 600;
+        setSize(høyde, bredde);
     }
+
+    /*public void byttLabels(JLabel l1, JLabel l2)
+    {
+		l1.setVisible(false);
+		l2.setVisible(true);
+	}*/
 
     public void redigerProfil()
     {
 		rediger.setEnabled(false);
         lagre.setEnabled(true);
-		fnavn.setVisible(true);
-		enavn.setVisible(true);
-		fonavn.setVisible(false);
-		etnavn.setVisible(false);
-		tlf.setVisible(false);
-		telf.setVisible(true);
-		avd.setVisible(false);
-		avde.setVisible(true);
+		info.removeAll();
+		info.add(idLabel);
+		info.add(ansid);
+		info.add(nrLabel);
+		info.add(persnr);
+		info.add(navnLabel);
+		info.add(fnavn);
+		info.add(enavn);
+		info.add(tlfLabel);
+		info.add(telf);
+		info.add(avdLabel);
+		info.add(avde);
+		info.revalidate();
+		info.repaint();
     }
 
     public void lagreProfil()
     {
-		ansatt.setFornavn(fonavn.getText());
-		ansatt.setEtternavn(etnavn.getText());
-		ansatt.setTelefonNr(telf.getText());
-		ansatt.setAvdeling(avde.getText());
+		ansatt.setFornavn(fnavn.getText());
+		ansatt.setEtternavn(enavn.getText());
+		ansatt.setTelefonNr(tlf.getText());
+		ansatt.setAvdeling(avd.getText());
+
+		navn = new JLabel(ansatt.getFornavn() + " " + ansatt.getEtternavn());
+		navn.setFont(font);
+		tlf = new JLabel(ansatt.getTelefonNr());
+		tlf.setFont(font);
+		avd = new JLabel(ansatt.getAvdeling());
+		avd.setFont(font);
 
 		lagre.setEnabled(false);
 		rediger.setEnabled(true);
-		fnavn.setVisible(true);
-		enavn.setVisible(true);
-		fonavn.setVisible(false);
-		etnavn.setVisible(false);
-		tlf.setVisible(true);
-		telf.setVisible(false);
-		avd.setVisible(true);
-		avde.setVisible(false);
+		info.removeAll();
+		info.add(idLabel);
+		info.add(ansid);
+		info.add(nrLabel);
+		info.add(persnr);
+		info.add(navnLabel);
+		info.add(navn);
+		info.add(tlfLabel);
+		info.add(tlf);
+		info.add(avdLabel);
+		info.add(avd);
+		info.revalidate();
+		info.repaint();
 	}
 
     private class MainListener implements ActionListener
@@ -176,11 +225,11 @@ public class AnsattProfil extends JFrame
         }
     }
 
-    /*private class WinListener extends WindowAdapter
+    private class WinListener extends WindowAdapter
     {
         public void windowClosing(WindowEvent e)
         {
             register.exit();
         }
-    }*/
+    }
 }
