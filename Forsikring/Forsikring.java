22619e71175fa55cf32f8d48f0a1b28c7326121b
Even <evva-gangsta@hotmail.com>
2015-05-11 20:07:51 +0200

    Update Forsikring.java

--- Forsikring/Forsikring.java
+++ Forsikring/Forsikring.java
@@ -15,10 +15,30 @@ public abstract class Forsikring implements Serializable
 	private String medName;
 	private String category;
 	private int group;
-	private String forsikringsinfo;
+	protected String forsikringsinfo, ja = "Ja", nei = "Nei";
 
 	protected Forsikring(Kunde kun, String info)
 	{
+		/*
+		Kanskje ikke så smart å ha bokstav i forsikringsnummeret
+		ha 6-sifret nummer med forskjellig førstetall
+
+		i forsikringsvinduet kan det være en "Pris"-panel nederst hvor totalsum står
+		alle vinduene har en "meld skade"-knapp
+		eneste som kan forandres er kjørelengde
+		kunde kan se og forandre sin egen informasjon:
+		navn, fødselsdato, postnr, poststed, tlfnr
+
+		innbo forandre:
+		adresse, informasjon
+
+		hus forandre:
+		adresse, egenandel, informasjon
+
+		kunde kan se alle sine forsikringer bra på sin forside
+		i forsikringsvinduet kan oppsigelse bli sendt inn
+
+		*/
 		kunde = kun;
 		forsikringsinfo = info;
 		inngått = Calendar.getInstance();
@@ -278,9 +298,10 @@ class BilForsikring extends Fartøy implements Serializable
 	static final long serialVersionUID = 42L;
 	private String eiernavn, registreringsnr, biltype, modell, bilfornr;
 	private int registreringsår, kjørelengde, prisPrKm, hjelpenr;
-	private static int nestenr = 1;
+	private static int nestenr = 20000;
+	private boolean aktiv;
 
-	public BilForsikring(Kunde kun, int beløp, String info, String eier, String regnr, String typ, String mod, int regår, int lengde, int prkm)
+	public BilForsikring(Kunde kun, String info, String eier, String regnr, String typ, String mod, int regår, int lengde, int prkm, int beløp )
 	{
 		super(kun, beløp, info, eier, regnr, typ, mod);
 		registreringsår = regår;
@@ -288,6 +309,7 @@ class BilForsikring extends Fartøy implements Serializable
 		prisPrKm = prkm;
 		hjelpenr = nestenr;
 		nestenr++;
+		aktiv = true;
 	}
 
 	public int getRegistreringsår()
@@ -325,15 +347,32 @@ class BilForsikring extends Fartøy implements Serializable
 		return hjelpenr;
 	}
 
-	public String getForsikringsNr()
+	public int getForsikringsNr()
 	{
-		bilfornr = Integer.toString(hjelpenr);
-		return "BI" + bilfornr;
+		return hjelpenr;
 	}
 
 	public int getBonus()
 	{
 		return 25;
+	}
+
+	public boolean getAktiv()
+	{
+		return aktiv;
+	}
+
+	public String getErAktiv()
+	{
+		if(aktiv)
+			return ja;
+		else
+			return nei;
+	}
+
+	public void setAktiv(boolean ok)
+	{
+		aktiv = ok;
 	}
 
 	public static int getNrNå()
@@ -363,7 +402,8 @@ class BåtForsikring extends Fartøy implements Serializable
 	static final long serialVersionUID = 42L;
 	private String eiernavn, registreringsnr, biltype, modell, båtfornr, motortype;
 	private int årsmodell, båtlengde, motorstyrke, hjelpenr;
-	private static int nestenr = 1;
+	private static int nestenr = 30000;
+	private boolean aktiv;
 
 	public BåtForsikring(Kunde kun, int beløp, String info, String eier, String regnr, String typ, String mod, int modellår, int lengde, String motortyp, int styrke)
 	{
@@ -374,6 +414,7 @@ class BåtForsikring extends Fartøy implements Serializable
 		motorstyrke = styrke;
 		hjelpenr = nestenr;
 		nestenr++;
+		aktiv = true;
 	}
 
 	public int getÅrsmodell()
@@ -421,10 +462,27 @@ class BåtForsikring extends Fartøy implements Serializable
 		return hjelpenr;
 	}
 
-	public String getForsikringsnr()
+	public int getForsikringsnr()
 	{
-		båtfornr = Integer.toString(hjelpenr);
-		return "BA" + båtfornr;
+		return hjelpenr;
+	}
+
+	public boolean getAktiv()
+	{
+		return aktiv;
+	}
+
+	public String getErAktiv()
+	{
+		if(aktiv)
+			return ja;
+		else
+			return nei;
+	}
+
+	public void setAktiv(boolean ok)
+	{
+		aktiv = ok;
 	}
 
 	public static int getNrNå()
@@ -455,13 +513,15 @@ class HusForsikring extends Bolig implements Serializable
 	static final long serialVersionUID = 42L;
 	private String husfornr;
 	private int hjelpenr;
-	private static int nestenr = 1;
+	private static int nestenr = 40000;
+	private boolean aktiv;
 
 	public HusForsikring(Kunde kun, int beløpb, int beløpi, String info, String adr, String type, String mat, String stand, int år, int meter)
 	{
 		super(kun, beløpb, beløpi, info, adr, type, mat, stand, år, meter);
 		hjelpenr = nestenr;
 		nestenr++;
+		aktiv = true;
 	}
 
 	public int getForsikringsID()
@@ -469,10 +529,27 @@ class HusForsikring extends Bolig implements Serializable
 		return hjelpenr;
 	}
 
-	public String getForsikringsnr()
+	public int getForsikringsnr()
 	{
-		husfornr = Integer.toString(hjelpenr);
-		return "HU" + husfornr;
+		return hjelpenr;
+	}
+
+	public boolean getAktiv()
+	{
+		return aktiv;
+	}
+
+	public String getErAktiv()
+	{
+		if(aktiv)
+			return ja;
+		else
+			return nei;
+	}
+
+	public void setAktiv(boolean ok)
+	{
+		aktiv = ok;
 	}
 
 	public static int getNrNå()
@@ -503,13 +580,15 @@ class HytteForsikring extends Bolig implements Serializable
 	static final long serialVersionUID = 42L;
 	private String hyttefornr;
 	private int hjelpenr;
-	private static int nestenr = 1;
+	private static int nestenr = 50000;
+	private boolean aktiv;
 
 	public HytteForsikring(Kunde kun, int beløpb, int beløpi, String info, String adr, String type, String mat, String stand, int år, int meter)
 	{
 		super(kun, beløpb, beløpi, info, adr, type, mat, stand, år, meter);
 		hjelpenr = nestenr;
 		nestenr++;
+		aktiv = true;
 	}
 
 	public int getForsikringsID()
@@ -517,10 +596,27 @@ class HytteForsikring extends Bolig implements Serializable
 		return hjelpenr;
 	}
 
-	public String getForsikringsnr()
+	public int getForsikringsnr()
 	{
-		hyttefornr = Integer.toString(hjelpenr);
-		return "HY" + hyttefornr;
+		return hjelpenr;
+	}
+
+	public boolean getAktiv()
+	{
+		return aktiv;
+	}
+
+	public String getErAktiv()
+	{
+		if(aktiv)
+			return ja;
+		else
+			return nei;
+	}
+
+	public void setAktiv(boolean ok)
+	{
+		aktiv = ok;
 	}
 
 	public static int getNrNå()
