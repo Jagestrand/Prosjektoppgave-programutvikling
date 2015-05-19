0be182b8b4697ef023ecc20f9eb35644bd5217bf
Jagestrand <Jagestrand.william@outlook.com>
2015-05-19 19:10:47 +0200

    Update KundeGUI.java

--- Forsikring/KundeGUI.java
+++ Forsikring/KundeGUI.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
-
+import javax.swing.border.MatteBorder;
+import java.awt.image.*;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -9,97 +10,147 @@ public class KundeGUI extends JPanel
 	private Register register;
 	private Kunde kunde;
 	private Huvudvindu vindu;
+	//private Lytterklasse lytter;
 	private int DATA_FIELD_LENGTH = 20;
+	private String printNavn, printEtternavn, printTelefon, printAdresse, printPrnmr, printPoststed, printPostNmr;
 	private JTextField navn, telefon, adresse, personnr;
 	private JTextArea informationTop, visForsikringInfo;
-	private JButton skademeldinger, regForsikring, båt, bil, hus, fritid;
-	private JLabel navnLabel, telefonLabel, adresseLabel, personnrLabel;
-	private JPanel border, border2, flow, registerGrid, forsikringsGrid;
-	private TModel tableModel;
+	private JButton visSkademelding, regSkademelding, regForsikring, båtKnapp, bilKnapp, husKnapp, fritidKnapp;
+	private JLabel inloggadSom, navnLabel, etternavnLabel, telefonLabel, adresseLabel, postStedLabel, postNmrLabel, personnrLabel;
+	private JPanel border, borderLeft, borderRight, flow, flowLeft, flowRight,registerGrid, registerGridLeft, registerGridRight;
 	private JTable table;
-	//public final static String typeForsikring = "Båt";
-	//private Lytterklasse lytter;
-
+	private MineForsikringer mineforsikringer;
+	
 	public KundeGUI(Huvudvindu v, Kunde kunn)
 	{
-		/*
-		knapp henter fram info i det store feltet/egen popoppboks
-		det står "Innlogget: <Kundenavn>" oppe i venstre hjørne
-		har en "meld skade"-knapp og "si opp forsikring"-knapp og en "skadesaker"-knapp
-		"skadesaker-knapp" leder til liste med skademeldinger
-		*/
-
 		vindu = v;
 		kunde = kunn;
-	//	lytter = new Lytterklasse();
 		register = vindu.getRegister();
-
+		//lytter = new Lytterklasse();
+		mineforsikringer = new MineForsikringer(kunde.getBiler(),kunde.getBåter(),kunde.getHus(),kunde.getHytter());
+		
 		setLayout(new BorderLayout() );
-
 		border = new JPanel(new BorderLayout());
-		registerGrid = new JPanel(new GridLayout(10,1));
-		flow = new JPanel( new FlowLayout());
-		/*String[] comboBoxItem = {typeForsikring};
-		JComboBox cb = new JComboBox(comboBoxItem);
-		cb.setEditable(false);
-		cb.addActionListener(this);*/
+		GridLayout gridlayout = new GridLayout(12, 1);
 
+		//gridlayout.setVgap(10);
+		/*BackgroundPanel panel = new BackgroundPanel(duke, BackgroundPanel.ACTUAL, 1.0f, 0.5f);
+		GradientPaint paint = new GradientPaint(0, 0, Color.BLUE, 600, 0, Color.RED);
+			panel.setPaint(paint);*/
+		registerGrid = new JPanel(gridlayout);
+		flow = new JPanel( new FlowLayout());	
+		
+		//Panes vänster
+		borderLeft = new JPanel(new BorderLayout());
+		GridLayout gridlayoutLeft = new GridLayout(12, 1);
+		
+		registerGridLeft = new JPanel(gridlayoutLeft);
+		flowLeft = new JPanel( new FlowLayout());
+		
+		//Panels höger
+		borderRight = new JPanel(new BorderLayout());
+		GridLayout gridlayoutRight = new GridLayout(12, 1);
+		
+		registerGridRight = new JPanel(gridlayoutRight);
+		flowRight = new JPanel( new FlowLayout());
+		
+		//Kund info högerpanel
+		printNavn = kunde.getFornavn();
+		printEtternavn = kunde.getEtternavn();
+		printTelefon = kunde.getTelefonNr();
+		printAdresse = kunde.getAdresse();
+		printPoststed = kunde.getPoststed();
+		printPostNmr = kunde.getPostnr();
+		printPrnmr = kunde.getPersonNr();
+		
+		inloggadSom = new JLabel("Kund information: ");
+		navnLabel = new JLabel("Navn: " + " " + printNavn);
+		etternavnLabel = new JLabel("Etternavn: " + " " + printEtternavn);
+		telefonLabel = new JLabel("Telefon: " + " " + printTelefon);
+		adresseLabel = new JLabel("Adresse: " + " " + printAdresse);
+		postStedLabel = new JLabel("Poststed: " + " " + printPoststed);
+		postNmrLabel = new JLabel("Postnummer: " + " " + printPostNmr);
+		personnrLabel = new JLabel("Personnummer:" + " " + printPrnmr);
+		
+		inloggadSom.setFont(new Font("Serif", Font.BOLD, 14));
+		inloggadSom.setForeground(Color.RED);
+		navnLabel.setFont(new Font("Serif", Font.ITALIC, 14));
+		etternavnLabel.setFont(new Font("Serif", Font.ITALIC, 14));
+		telefonLabel.setFont(new Font("Serif", Font.ITALIC, 14));
+		adresseLabel.setFont(new Font("Serif", Font.ITALIC, 14));
+		postStedLabel.setFont(new Font("Serif", Font.ITALIC, 14));
+		postNmrLabel.setFont(new Font("Serif", Font.ITALIC, 14));
+		personnrLabel.setFont(new Font("Serif", Font.ITALIC, 14));
+		
+		//knappar på vänstergrid
+		regForsikring = new JButton("Registrere forsikringer");
+		//båtKnapp = new JButton("Mine båtforsikringer");
+		//bilKnapp = new JButton("Mine bilforsikringer");
+		//husKnapp = new JButton("Mine husforsikringer");
+		//fritidKnapp = new JButton("Mine hytteforsikringer");*/
+		
+		registerGridRight.add(regForsikring);
+	//	registerGridLeft.add(båtKnapp);
+	//	registerGridLeft.add(bilKnapp);
+	//	registerGridLeft.add(husKnapp);
+	//	registerGridLeft.add(fritidKnapp);
+	//		regForsikring.addActionListener(lytter);
+		
+		regSkademelding = new JButton("Registrere skademeldinger");
+		visSkademelding = new JButton("Se skademeldinger");
 
+		//registerGridLeft.add(regSkademelding);
+		//registerGridLeft.add(visSkademelding);
 
-		//infoormations felt
-		informationTop = new JTextArea("Velkommen tilbake");
+	//	regSkademelding.addActionListener(lytter);
+	//	visSkademelding.addActionListener(lytter);
 
-		informationTop.setLineWrap(true);
-		informationTop.setWrapStyleWord(true);
-		informationTop.setEditable(false);
+		registerGridRight.add(inloggadSom);
+		registerGridRight.add(navnLabel);
+		registerGridRight.add(etternavnLabel);
+		registerGridRight.add(telefonLabel);
+		registerGridRight.add(adresseLabel);
+		registerGridRight.add(postStedLabel);
+		registerGridRight.add(postNmrLabel);
+		registerGridRight.add(personnrLabel);
 
-		navn = new JTextField(DATA_FIELD_LENGTH);
-		telefon = new JTextField(DATA_FIELD_LENGTH);
-		adresse = new JTextField(DATA_FIELD_LENGTH);
-		personnr = new JTextField(DATA_FIELD_LENGTH);
-
-		navnLabel = new JLabel("Navn:");
-		telefonLabel = new JLabel("Telefon:");
-		adresseLabel = new JLabel("Adresse:");
-		personnrLabel = new JLabel("Personnummer:");
-
-		registerGrid.add(navnLabel);
-		registerGrid.add(navn);
-		registerGrid.add(telefonLabel);
-		registerGrid.add(telefon);
-		registerGrid.add(adresseLabel);
-		registerGrid.add(adresse);
-		registerGrid.add(personnrLabel);
-		registerGrid.add(personnr);
-
+		
+		registerGridLeft.add(mineforsikringer);
+		
 		border.add(registerGrid, BorderLayout.LINE_START);
-		flow.add(border);
-		add(informationTop, BorderLayout.PAGE_START);
+		flow.add(registerGrid);
+
 		add( new JScrollPane(flow, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER)
-		,BorderLayout.LINE_START);
-		tableModel = new TModel(register.getAnsatte()); //new TModel()
-		table = new JTable(tableModel);
+		,BorderLayout.NORTH);
 		//legger til elementer i hovedpanelet
 		add(new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), BorderLayout.CENTER);
+		
+		borderLeft.add(registerGridLeft, BorderLayout.WEST);
+		flowLeft.add(borderLeft);
+		
+		add( new JScrollPane(flowLeft, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER)
+		,BorderLayout.WEST);
+		//legger til elementer i hovedpanelet
+		
+		borderRight.add(registerGridRight, BorderLayout.EAST);
+		flowRight.add(borderRight);
+		
+		add( new JScrollPane(flowRight, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER)
+		,BorderLayout.EAST);
+		//legger til elementer i hovedpanelet
+		
 
-
-		regForsikring = new JButton("Registrere forsikringer");
-		registerGrid.add(regForsikring);
 		regForsikring.addActionListener (new Action1());
-
-		skademeldinger = new JButton("Registrere skademeldinger");
-		registerGrid.add(skademeldinger);
-
+	    
 	}
-		class Action1 implements ActionListener
+	
+	class Action1 implements ActionListener
+	{
+	public void actionPerformed (ActionEvent e)
 		{
-		public void actionPerformed (ActionEvent e)
-			{
-				ValgAvForsikring cl = new ValgAvForsikring(vindu, kunde);
-				//  cl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				cl.setVisible(true);
-			}
+			ValgAvForsikring cl = new ValgAvForsikring(vindu, kunde);
+			//  cl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			cl.setVisible(true);
 		}
-
-
+	}
 }
