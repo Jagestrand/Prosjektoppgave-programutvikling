53b62f8b97da751a8ab166232d1a8bea81e8c1b3
Even <evva-gangsta@hotmail.com>
2015-05-20 12:32:56 +0200

    Update ForsikringsReg.java

--- Forsikring/ForsikringsReg.java
+++ Forsikring/ForsikringsReg.java
@@ -1,4 +1,4 @@
-/*
+/*Skrevet av Even Nerheim, s199184, sist redigert 16.05.2015
 Denne klassen er bare til for å skille mellom forsikringene
 */
 
@@ -10,14 +10,22 @@ public class ForsikringsReg implements Serializable
 {
 	private static final long serialVersionUID = 42L;
 	private Forsikring1 f;
-	private ForsikringsReg1 forReg;
-	private BilForsikringsReg bilReg;
-	private BåtForsikringsReg båtReg;
-	private HusForsikringsReg husReg;
-	private HytteForsikringsReg hytReg;
+	private BilForsikringsReg bil;
+	private BåtForsikringsReg båt;
+	private HusForsikringsReg hus;
+	private HytteForsikringsReg hyt;
 
 	public ForsikringsReg()
 	{
+		bil = new BilForsikringsReg();
+		båt = new BåtForsikringsReg();
+		hus = new HusForsikringsReg();
+		hyt = new HytteForsikringsReg();
+	}
+
+	public int size()
+	{
+		return bil.size() + båt.size() + hus.size() + hyt.size();
 	}
 
 	public int sjekkForsikringsNr(int nr)
@@ -49,4 +57,6 @@ public class ForsikringsReg implements Serializable
 
 		return 0;
 	}
+
+
 }
