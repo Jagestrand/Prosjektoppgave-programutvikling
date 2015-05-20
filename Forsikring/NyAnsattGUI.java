69d21ace4b3a3a81eb563eb70c71da2cb0853eff
Even <evva-gangsta@hotmail.com>
2015-05-20 12:37:06 +0200

    Update NyAnsattGUI.java

--- Forsikring/NyAnsattGUI.java
+++ Forsikring/NyAnsattGUI.java
@@ -1,6 +1,11 @@
+/*Skrevet av Even Nerhei, s199184, sist redigert 11.05.2015
+Vindu for å registrere nye ansatte i bedriften og dermed også nye brukere.
+Kun admin får adgang til denne siden
+*/
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;;
+import java.awt.event.*;
 import javax.swing.border.*;
 
 public class NyAnsattGUI extends JPanel
@@ -9,66 +14,68 @@ public class NyAnsattGUI extends JPanel
 	private boolean a = false, b = false, c = false;
 	private Huvudvindu win;
 	private Register reg;
-	private JTextField fName, lName, pNr, pass1, pass2, phone, empAt;
+	private JTextField fNavn, eNavn, pNr, pass1, pass2, tlf, ansAt;
 	private JTextArea info;
-	private JLabel fNameLabel, lNameLabel, pNrLabel, pass1Label, pass2Label, phoneLabel, empAtLabel;
+	private JLabel fNavnLabel, eNavnLabel, pNrLabel, pass1Label, pass2Label, tlfLabel, ansAtLabel;
 	private JButton addAns;
-	private Listener listener;
+	private Lytterklasse lytter;
 	private JPanel fieldGrid, flow;
 
 	public NyAnsattGUI(Huvudvindu w)
 	{
 		win = w;
 		reg = w.getRegister();
-		listener = new Listener();
+		lytter = new Lytterklasse();
+
 		//oppretter ulike jpaneler med ulike lyoutmanagere
 		setLayout(new BorderLayout() );
-		fieldGrid = new JPanel(new GridLayout(17, 1) );//ser ingen grunn til å opprette disse tallene som egne konstanter
-		//
+		fieldGrid = new JPanel(new GridLayout(17, 1) );
 		fieldGrid.setBorder(new EmptyBorder(new Insets(30, 0, 0, 0)));
-		//
 		flow = new JPanel(new FlowLayout() );
+
 		//oppretter info feltet
 		info = new JTextArea();
 		info.setEditable(false);
 		info.setText("Fyll ut feltene for å registrere en ny ansatt. Personnummeret skal være 11 siffer, og passordene må være like.\n" +
 						"Klikk på Lagre ansatt når du er ferdig");
+
 		//oppretter data feltene og lagre knapp
-		fName = new JTextField(DATA_FIELD_LENGTH);
-		lName = new JTextField(DATA_FIELD_LENGTH);
+		fNavn = new JTextField(DATA_FIELD_LENGTH);
+		eNavn = new JTextField(DATA_FIELD_LENGTH);
 		pNr = new JTextField(DATA_FIELD_LENGTH);
 		pass1 = new JPasswordField(DATA_FIELD_LENGTH);
 		pass2 = new JPasswordField(DATA_FIELD_LENGTH);
-		phone = new JTextField(DATA_FIELD_LENGTH);
-		empAt = new JTextField(DATA_FIELD_LENGTH);
-		fNameLabel = new JLabel("Fornavn:");
-		lNameLabel = new JLabel("Etternavn:");
+		tlf = new JTextField(DATA_FIELD_LENGTH);
+		ansAt = new JTextField(DATA_FIELD_LENGTH);
+		fNavnLabel = new JLabel("Fornavn:");
+		eNavnLabel = new JLabel("Etternavn:");
 		pNrLabel = new JLabel("Personnummer:");
 		pass1Label = new JLabel("Login passord:");
 		pass2Label = new JLabel("Gjenta passord:");
-		phoneLabel = new JLabel("Telefonnummer:");
-		empAtLabel = new JLabel("Arbeidsplass:");
+		tlfLabel = new JLabel("Telefonnummer:");
+		ansAtLabel = new JLabel("Arbeidsplass:");
 		addAns = new JButton("Lagre ansatt");
 		//legger til lyttere
-		addAns.addActionListener(listener);
-		fName.addActionListener(listener);
-		lName.addActionListener(listener);
-		pNr.addActionListener(listener);
-		pass1.addActionListener(listener);
-		pass2.addActionListener(listener);
-		phone.addActionListener(listener);
-		empAt.addActionListener(listener);
+		addAns.addActionListener(lytter);
+		fNavn.addActionListener(lytter);
+		eNavn.addActionListener(lytter);
+		pNr.addActionListener(lytter);
+		pass1.addActionListener(lytter);
+		pass2.addActionListener(lytter);
+		tlf.addActionListener(lytter);
+		ansAt.addActionListener(lytter);
+
 		//legger til elementer i paneler
-		fieldGrid.add(fNameLabel);
-		fieldGrid.add(fName);
-		fieldGrid.add(lNameLabel);
-		fieldGrid.add(lName);
+		fieldGrid.add(fNavnLabel);
+		fieldGrid.add(fNavn);
+		fieldGrid.add(eNavnLabel);
+		fieldGrid.add(eNavn);
 		fieldGrid.add(pNrLabel);
 		fieldGrid.add(pNr);
-		fieldGrid.add(empAtLabel);
-		fieldGrid.add(empAt);
-		fieldGrid.add(phoneLabel);
-		fieldGrid.add(phone);
+		fieldGrid.add(ansAtLabel);
+		fieldGrid.add(ansAt);
+		fieldGrid.add(tlfLabel);
+		fieldGrid.add(tlf);
 		fieldGrid.add(pass1Label);
 		fieldGrid.add(pass1);
 		fieldGrid.add(pass2Label);
@@ -79,15 +86,15 @@ public class NyAnsattGUI extends JPanel
 		add(info, BorderLayout.CENTER);
 	}
 
-	public void nyAnsatt()
+	public void nyAnsatt()				//metode for å sjekke om feltene er riktig fylt ut og hvis de er, registrerer ny ansattbruker
 	{
 		String fn, ln, pn, p, t, ea, regexPattern, ansnr;
 		regexPattern = "(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])(\\d{7})";
-		fn = fName.getText();
-		ln = lName.getText();
+		fn = fNavn.getText();
+		ln = eNavn.getText();
 		pn = pNr.getText();
-		t = phone.getText();
-		ea = empAt.getText();
+		t = tlf.getText();
+		ea = ansAt.getText();
 		p = pass1.getText();
 		if(!p.equals(pass2.getText() ) )
 			info.setText("Passordene må være like");
@@ -105,13 +112,13 @@ public class NyAnsattGUI extends JPanel
 		}
 	}
 
-	private class Listener implements ActionListener
+	private class Lytterklasse implements ActionListener		//lytter til knapp og felter, så man ikke nødvendigvis må trykke på knappen
 	{
 		public void actionPerformed(ActionEvent e)
 		{
-			if(e.getSource() == addAns || e.getSource() == fName || e.getSource() == lName
+			if(e.getSource() == addAns || e.getSource() == fNavn || e.getSource() == eNavn
 			|| e.getSource() == pNr || e.getSource() == pass1 || e.getSource() == pass2
-			 || e.getSource() == phone || e.getSource() == empAt)
+			 || e.getSource() == tlf || e.getSource() == ansAt)
 				nyAnsatt();
 		}
 	}
