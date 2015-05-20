a5fc4c78ef3975a143a87cb36eb1de5587b09e71
Even <evva-gangsta@hotmail.com>
2015-05-20 13:16:39 +0200

    Update KundeGUI.java

--- Forsikring/KundeGUI.java
+++ Forsikring/KundeGUI.java
@@ -12,7 +12,7 @@ public class KundeGUI extends JPanel
 	private Huvudvindu vindu;
 	private Lytterklasse lytter;
 	private String printNavn, printEtternavn, printTelefon, printAdresse, printPrnmr, printPoststed, printPostNmr;
-	private JButton regSkademelding, regForsikring, deaktiverF;
+	private JButton regSkademelding, regForsikring, deaktiverF, oppdaterKnapp;
 	private JLabel inloggadSom, navnLabel, etternavnLabel, telefonLabel, adresseLabel, postStedLabel, postNmrLabel, personnrLabel;
 	private JPanel border, borderLeft, borderRight, flow, flowLeft, flowRight,registerGrid, registerGridLeft, registerGridRight;
 	private JTabbedPane tabbedPane2;
@@ -24,18 +24,18 @@ public class KundeGUI extends JPanel
 		vindu = v;
 		kunde = kunn;
 		register = vindu.getRegister();
-		
+
 		//Oppretter lyttere
 		lytter = new Lytterklasse();
-		
+
 		//Setter layout
 		setLayout(new BorderLayout() );
 		border = new JPanel(new BorderLayout());
 		GridLayout gridlayout = new GridLayout(12, 1);
-		
+
 		//oppretter JPaneler med ulike LayoutManagere
 		registerGrid = new JPanel(gridlayout);
-		flow = new JPanel( new FlowLayout());	
+		flow = new JPanel( new FlowLayout());
 		//Panes til WEST
 		borderLeft = new JPanel(new BorderLayout());
 		GridLayout gridlayoutLeft = new GridLayout(12, 1);
@@ -44,7 +44,7 @@ public class KundeGUI extends JPanel
 		//Panels til EAST
 		borderRight = new JPanel(new BorderLayout());
 		GridLayout gridlayoutRight = new GridLayout(12, 1);
-		
+
 		registerGridRight = new JPanel(gridlayoutRight);
 		flowRight = new JPanel( new FlowLayout());
 
@@ -56,7 +56,7 @@ public class KundeGUI extends JPanel
 		printPoststed = kunde.getPoststed();
 		printPostNmr = kunde.getPostnr();
 		printPrnmr = kunde.getPersonNr();
-		
+
 		//Setter informationen till en Jlabel for visning
 		inloggadSom = new JLabel("Kund information: ");
 		navnLabel = new JLabel("Navn: " + " " + printNavn);
@@ -66,7 +66,7 @@ public class KundeGUI extends JPanel
 		postStedLabel = new JLabel("Poststed: " + " " + printPoststed);
 		postNmrLabel = new JLabel("Postnummer: " + " " + printPostNmr);
 		personnrLabel = new JLabel("Personnummer:" + " " + printPrnmr);
-		
+
 		//Setter fontstyle och ferg på den printade teksten
 		inloggadSom.setFont(new Font("Serif", Font.BOLD, 14));
 		inloggadSom.setForeground(Color.RED);
@@ -77,7 +77,7 @@ public class KundeGUI extends JPanel
 		postStedLabel.setFont(new Font("Serif", Font.ITALIC, 14));
 		postNmrLabel.setFont(new Font("Serif", Font.ITALIC, 14));
 		personnrLabel.setFont(new Font("Serif", Font.ITALIC, 14));
-		
+
 		//legger elementer til i GUI
 		registerGridRight.add(inloggadSom);
 		registerGridRight.add(navnLabel);
@@ -87,17 +87,17 @@ public class KundeGUI extends JPanel
 		registerGridRight.add(postStedLabel);
 		registerGridRight.add(postNmrLabel);
 		registerGridRight.add(personnrLabel);
-		
+
 		//Skapar knapparna till panelen på venster sida
 		deaktiverF = new JButton ("Deaktiver forsikring");
 		regForsikring = new JButton("Registrere forsikring");
 		regSkademelding = new JButton("Registrere skademeldinger");
-		
+
 		//Legger in knapparna till panelen på venster sida
 		registerGridLeft.add(regForsikring);
-		registerGridLeft.add(deaktiverF);	
+		registerGridLeft.add(deaktiverF);
 		registerGridLeft.add(regSkademelding);
-		
+
 		//Legger til elementer i hovedpanelet
 		border.add(registerGrid, BorderLayout.LINE_START);
 		flow.add(registerGrid);
@@ -107,12 +107,12 @@ public class KundeGUI extends JPanel
 		borderLeft.add(registerGridLeft, BorderLayout.WEST);
 		flowLeft.add(borderLeft);
 		add( new JScrollPane(flowLeft, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER)
-		,BorderLayout.WEST);		
+		,BorderLayout.WEST);
 		borderRight.add(registerGridRight, BorderLayout.EAST);
 		flowRight.add(borderRight);
 		add( new JScrollPane(flowRight, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER)
 		,BorderLayout.EAST);
-		
+
 		//Oppretter tabellen for visning
 		tableModel21 = new TModel(kunde.getBiler());
 		table21 = new JTable(tableModel21);
@@ -125,7 +125,7 @@ public class KundeGUI extends JPanel
 		tableModel25 = new TModel(kunde.getSkademeldinger());
 		table25 = new JTable(tableModel25);
 
-		
+
 		tabbedPane2 = new JTabbedPane();
 		tabbedPane2.addTab("Bil", null, (new JScrollPane(table21, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED)), "Liste over biler");
 		tabbedPane2.setMnemonicAt(0, KeyEvent.VK_1);
@@ -137,24 +137,23 @@ public class KundeGUI extends JPanel
 		tabbedPane2.setMnemonicAt(3, KeyEvent.VK_4);
 		tabbedPane2.addTab("Skademelding", null, (new JScrollPane(table25, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED)), "Liste over skader");
 		tabbedPane2.setMnemonicAt(3, KeyEvent.VK_4);
-		
+
 		add(new JScrollPane(tabbedPane2, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), BorderLayout.CENTER);
-		
+
 		//Binder lytter till knappar
 		regForsikring.addActionListener (lytter);
 		regSkademelding.addActionListener(lytter);
-		deaktiverF.addActionListener(lytter);	
+		deaktiverF.addActionListener(lytter);
 	}
-	
-	//Kallar på registrerings GUI 
+
+	//Kallar på registrerings GUI
 	public void nyForsikring()
 	{
 		ValgAvForsikring cl = new ValgAvForsikring(vindu, kunde);
 		cl.setVisible(true);
 	}
-	
-	//Metode for og deaktivera forsikring
-	public void deaktiverF()
+
+	public void nySkademeldingBil()
 	{
 		try
 		{
@@ -162,19 +161,136 @@ public class KundeGUI extends JPanel
 			if(row == -1)
 				return;
 
-			BilForsikring bilReg = register.getBilViaRegNr( (String)tableModel.getValueAt(row, TModel.PERSON_NR) );
-			if(!bilReg.getAktiv())
+          	BilForsikring bill = register.getBilViaNr( (int)tableModel21.getValueAt(row, TModel.FORSIKRINGS_NR) );
+          	new SkadeMeldingVindu(vindu, kunde, bill, null, null, null);
+        }
+        catch(NoSuchElementException nsee)
+        {
+			JOptionPane.showMessageDialog(null, "NoSuchElementException i nySkademeldingBil-metoden", "FEIL", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	public void nySkademeldingBåt()
+	{
+		try
+		{
+			int row = table22.getSelectedRow();
+			if(row == -1)
 				return;
+
+          	BåtForsikring bått = register.getBåtViaNr( (int)tableModel22.getValueAt(row, TModel.FORSIKRINGS_NR) );
+          	new SkadeMeldingVindu(vindu, kunde, null, bått, null, null);
+        }
+        catch(NoSuchElementException nsee)
+        {
+			JOptionPane.showMessageDialog(null, "NoSuchElementException i nySkademeldingBåt-metoden", "FEIL", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	public void nySkademeldingHus()
+	{
+		try
+		{
+			int row = table23.getSelectedRow();
+			if(row == -1)
+				return;
+
+          	HusForsikring huss = register.getHusViaNr( (int)tableModel23.getValueAt(row, TModel.FORSIKRINGS_NR) );
+          	new SkadeMeldingVindu(vindu, kunde, null, null, huss, null);
+        }
+        catch(NoSuchElementException nsee)
+        {
+			JOptionPane.showMessageDialog(null, "NoSuchElementException i nySkademeldingHus-metoden", "FEIL", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	public void nySkademeldingHytte()
+	{
+		try
+		{
+			int row = table24.getSelectedRow();
+			if(row == -1)
+				return;
+
+          	HytteForsikring hyttt = register.getHytteViaNr( (int)tableModel24.getValueAt(row, TModel.FORSIKRINGS_NR) );
+          	new SkadeMeldingVindu(vindu, kunde, null, null, null, hyttt);
+        }
+        catch(NoSuchElementException nsee)
+        {
+			JOptionPane.showMessageDialog(null, "NoSuchElementException i nySkademeldingBil-metoden", "FEIL", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	//Metode for og deaktivera forsikring
+	public void deaktiverF()
+	{
+		try
+		{
 			String melding = "Er du sikker på at du vil deaktivere forsikringen?";
 			String tittel = "Validering";
 
-			int svar = JOptionPane.showConfirmDialog(null, melding, tittel, JOptionPane.YES_NO_OPTION);
-			if(svar == JOptionPane.YES_OPTION)
-			{
-				register.deaktiverF(bilReg);
-				
+			int row = table21.getSelectedRow();
+			if(row == -1)
 				return;
+
+			if(!(register.getBilViaNr( (int)tableModel21.getValueAt(row, TModel.FORSIKRINGS_NR) ) == null))
+			{
+				BilForsikring forsikring = register.getBilViaNr( (int)tableModel21.getValueAt(row, TModel.FORSIKRINGS_NR) );
+				if(!forsikring.getAktiv())
+					return;
+				int svar = JOptionPane.showConfirmDialog(null, melding, tittel, JOptionPane.YES_NO_OPTION);
+				if(svar == JOptionPane.YES_OPTION)
+				{
+					register.avsluttForsikring(forsikring);
+					return;
+				}
 			}
+			row = table22.getSelectedRow();
+			if(row == -1)
+				return;
+
+			else if(!(register.getBåtViaNr( (int)tableModel22.getValueAt(row, TModel.FORSIKRINGS_NR) ) == null))
+			{
+				BåtForsikring forsikring = register.getBåtViaNr( (int)tableModel22.getValueAt(row, TModel.FORSIKRINGS_NR) );
+				if(!forsikring.getAktiv())
+					return;
+				int svar = JOptionPane.showConfirmDialog(null, melding, tittel, JOptionPane.YES_NO_OPTION);
+				if(svar == JOptionPane.YES_OPTION)
+				{
+					register.avsluttForsikring(forsikring);
+					return;
+				}
+			}
+			row = table23.getSelectedRow();
+			if(row == -1)
+				return;
+
+			else if(!(register.getHusViaNr( (int)tableModel23.getValueAt(row, TModel.FORSIKRINGS_NR) ) == null))
+			{
+				HusForsikring forsikring = register.getHusViaNr( (int)tableModel23.getValueAt(row, TModel.FORSIKRINGS_NR) );
+				if(!forsikring.getAktiv())
+					return;
+				int svar = JOptionPane.showConfirmDialog(null, melding, tittel, JOptionPane.YES_NO_OPTION);
+				if(svar == JOptionPane.YES_OPTION)
+				{
+					register.avsluttForsikring(forsikring);
+					return;
+				}
+			}
+			row = table24.getSelectedRow();
+			if(row == -1)
+				return;
+
+			else if(!(register.getHytteViaNr( (int)tableModel24.getValueAt(row, TModel.FORSIKRINGS_NR) ) == null))
+			{
+				HytteForsikring forsikring = register.getHytteViaNr( (int)tableModel24.getValueAt(row, TModel.FORSIKRINGS_NR) );
+				if(!forsikring.getAktiv())
+					return;
+				int svar = JOptionPane.showConfirmDialog(null, melding, tittel, JOptionPane.YES_NO_OPTION);
+				if(svar == JOptionPane.YES_OPTION)
+				{
+					register.avsluttForsikring(forsikring);
+					return;
+				}
+			}
+
 		}
 		catch(NoSuchElementException nsee)
 		{
@@ -190,11 +306,29 @@ public class KundeGUI extends JPanel
 			if(e.getSource() == regForsikring)
 				nyForsikring();
 			else if(e.getSource() == regSkademelding)
-		//		nySkademelding();
-		//	else if(e.getSource() == deaktiverF)
+			{
+				if(tabbedPane2.getSelectedIndex() == 0)
+					nySkademeldingBil();
+				else if(tabbedPane2.getSelectedIndex() == 1)
+					nySkademeldingBåt();
+				else if(tabbedPane2.getSelectedIndex() == 2)
+					nySkademeldingHus();
+				else if(tabbedPane2.getSelectedIndex() == 3)
+					nySkademeldingHytte();
+			}
+			else if(e.getSource() == deaktiverF)
 				deaktiverF();
-		//	else if(e.getSource() == visSkademelding)
-		//		seSkademelding();
+			/*else if(e.getSource() == visSkademelding)
+			{
+				if(tabbedPane2.getSelectedIndex() == 0)
+					seSkademeldingBil();
+				else if(tabbedPane2.getSelectedIndex() == 1)
+					seSkademeldingBåt();
+				else if(tabbedPane2.getSelectedIndex() == 2)
+					seSkademeldingHus();
+				else if(tabbedPane2.getSelectedIndex() == 3)
+					seSkademeldingHytte();
+			}*/
 		}
 	}
 }
